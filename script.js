8b49cd3871a1a8901d9aed306750c0c48970b7a5
BuwieSit <santosbuhawie28@gmail.com>
2025-04-17 07:44:56 +0800

    Responsive landing page

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const rightDiv = document.querySelector(".left");
 
 function leftLink() {
 
-    alert("WEBPAGE 1 IS WORKING");
-    //window.location.href = "market_vendor/web1_market.html";
+   
+    window.location.href = "market_vendor/web1_market.html";
 
 }
 
 function rightLink() {
-    alert("WEBPAGE 2 IS WORKING");
-    //window.location.href = "local_tourism/web2_market.html";
+   
+    window.location.href = "local_tourism/web2_tourism.html";
 }
