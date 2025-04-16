e966dae80f572cbec93cbfd07341286ba1a1f7b3
BuwieSit <santosbuhawie28@gmail.com>
2025-04-16 19:56:07 +0800

    fixed files and working links

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const leftDiv = document.querySelector(".left");
+const rightDiv = document.querySelector(".left");
+
+function leftLink() {
+
+    alert("WEBPAGE 1 IS WORKING");
+    //window.location.href = "market_vendor/web1_market.html";
+
+}
+
+function rightLink() {
+    alert("WEBPAGE 2 IS WORKING");
+    //window.location.href = "local_tourism/web2_market.html";
+}
