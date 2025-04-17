33c047defbeda7f2ddc7b2ba5c3854eee99e36bb
BuwieSit <santosbuhawie28@gmail.com>
2025-04-17 08:55:22 +0800

    temp removal of inactive tab stuff

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-
-const leftDiv = document.querySelector(".left");
-const rightDiv = document.querySelector(".left");
-
 function leftLink() {
-
-   
     window.location.href = "market_vendor/web1_market.html";
-
 }
-
 function rightLink() {
-   
     window.location.href = "local_tourism/web2_tourism.html";
 }
+
+const inactive = document.querySelector(".inactive");
+
