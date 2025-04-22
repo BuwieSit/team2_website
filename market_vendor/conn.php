0612bd8a98caf7be1a0820707da4435445810dc1
BuwieSit <santosbuhawie28@gmail.com>
2025-04-22 22:51:17 +0800

    landing page modifications, to continue login form

--- market_vendor/conn.php
+++ market_vendor/conn.php
@@ -1,31 +1,12 @@
 <?php
 
-    // try {
-    //     $conn = mysqli_connect('localhost', 'buwie', 'vendorReg_admin', 'market_vendor');
-    // }
-    // catch (mysqli_sql_exception) {
-    //         echo 'Connection Error: ' . mysqli_connect_error();
-  
-    // }
-    // if ($conn) {
-    //     echo "Connected";
-    // }
-    
-    $servername = "localhost";  // Or your database server
-    $username = "root";
-    $password = "";
-    $dbname = "market_vendor";
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    try {
+        $conn = mysqli_connect('localhost', 'buwie', 'vendorReg_admin', 'market_vendors');
     }
-
-    
-
+    catch (mysqli_sql_exception) {
+            echo 'Connection Error: ' . mysqli_connect_error();
+  
+    }
 
 
 
