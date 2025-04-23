e586de3a0d65e22256bf5de2427a1eac57f5f43c
BuwieSit <santosbuhawie28@gmail.com>
2025-04-23 13:32:18 +0800

    landing page design

--- market_vendor/landingScript.js
+++ market_vendor/landingScript.js
@@ -4,17 +4,20 @@ const sign2 = document.querySelector('.bs2');
 const login = document.querySelector('.login-up');
 const register = document.querySelector('.register-up');
 
+const logreg = document.querySelectorAll('.popup-group');
+const blackLayer = document.querySelector('.black-layer');
 
 sign1.addEventListener('click', () => {
     register.classList.remove('popUp');
     login.classList.add('popUp');
+    blackLayer.style.visibility = "visible";
 
 })
 
 sign2.addEventListener('click', () => {
     login.classList.remove('popUp');
     register.classList.add('popUp');
-    
+    blackLayer.style.visibility = "visible";
 
 })
 
@@ -30,8 +33,17 @@ formGroups.forEach(group => {
     inputEl.addEventListener('blur', () => {
         labelEl.style.transform = "translateX(0%)";
     });
-
-
 });
+
+logreg.forEach(logRegs => {
+    const close = logRegs.querySelector('.closeBtn');
+
+    close.addEventListener('click', () => {
+        register.classList.remove('popUp');
+        login.classList.remove('popUp');
+        blackLayer.style.visibility = "hidden";
+    })
+});
+
 
 
