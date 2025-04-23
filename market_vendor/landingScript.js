683f012c33076a24faeaea9718385b9fe9d1f75b
BuwieSit <santosbuhawie28@gmail.com>
2025-04-23 11:59:09 +0800

    form design complete, just adding a black layer

--- market_vendor/landingScript.js
+++ market_vendor/landingScript.js
@@ -1,9 +1,26 @@
 const formGroups = document.querySelectorAll('.form-grp');
+const sign1 = document.querySelector('.bs1');
+const sign2 = document.querySelector('.bs2');
+const login = document.querySelector('.login-up');
+const register = document.querySelector('.register-up');
+
+
+sign1.addEventListener('click', () => {
+    register.classList.remove('popUp');
+    login.classList.add('popUp');
+
+})
+
+sign2.addEventListener('click', () => {
+    login.classList.remove('popUp');
+    register.classList.add('popUp');
+    
+
+})
 
 formGroups.forEach(group => {
     const inputEl = group.querySelector('.form-input');
     const labelEl = group.querySelector('.input-label');
-
     inputEl.addEventListener('focus', () => {
         
         labelEl.style.transform = "translateX(30%)";
@@ -13,16 +30,8 @@ formGroups.forEach(group => {
     inputEl.addEventListener('blur', () => {
         labelEl.style.transform = "translateX(0%)";
     });
+
+
 });
-
-// inputEl.addEventListener("focus", () => {
-    
-//     labelEl.style.transform = "translateX(0%)";
-// });
-
-// inputEl.addEventListener("blur", () => {
-//     labelEl.style.transform = "translateX(30%)";
-// });
-
 
 
