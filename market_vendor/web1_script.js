2de1abe817eaaf2128201f7c56a6c31b034c7b39
BuwieSit <santosbuhawie28@gmail.com>
2025-04-27 03:06:26 +0800

    nav bar navigation design

--- market_vendor/web1_script.js
+++ market_vendor/web1_script.js
@@ -1,15 +1,32 @@
 
 const header = document.querySelector('.head');
-
-const headChildren = header.children;
+const navOptions = document.querySelectorAll('.nav-options');
+const linkOpt = document.querySelectorAll('.nav-options a');
 
 window.addEventListener('scroll', function() {
 
-    if (window.scrollY > 50) {
-   
+    if (window.scrollY > 100) {
         header.classList.add('scrolling');
     } else {
+    
         header.classList.remove('scrolling');
     }
 });
+
+navOptions.forEach(opt => {
+    opt.addEventListener('click', () => {
+
+        navOptions.forEach(o => o.style.boxShadow = 'none');
+
+        // Then, highlight the clicked one
+        opt.style.boxShadow = '0px 5px 10px black';
+    });
+});
+
+
+const formInputs = document.querySelectorAll(".vendor-form input");
+
+
+
 const vendorList = document.querySelector(".vendorList-cont");
+
